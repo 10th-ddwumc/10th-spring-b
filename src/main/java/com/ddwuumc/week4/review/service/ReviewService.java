61c1208f24d960abe58fe3b94926a2da31f3bbf2
mjttong <145860909+mjttong@users.noreply.github.com>
2026-04-28 22:55:06 +0900

    ✨ feat: 리뷰 작성 controller, dto 구현

--- src/main/java/com/ddwuumc/week4/review/service/ReviewService.java
+++ src/main/java/com/ddwuumc/week4/review/service/ReviewService.java
@@ -1,4 +1,17 @@
 package com.ddwuumc.week4.review.service;
 
+import com.ddwuumc.week4.review.dto.ReviewRequestDto;
+import com.ddwuumc.week4.review.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ReviewService {
+    
+    private final ReviewRepository reviewRepository;
+
+    public Long createReview(ReviewRequestDto.Review review) {
+        return 100L; // 생성한 리뷰의 review_id
+    }
 }
