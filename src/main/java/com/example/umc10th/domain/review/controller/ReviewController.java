3fe177d477daf5c517604ed92953d271ec7aebf0
yunnij <blrdyunnij@gmail.com>
2026-04-28 11:08:03 +0900

    [Feat] review 생성 api

--- src/main/java/com/example/umc10th/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/umc10th/domain/review/controller/ReviewController.java
@@ -1,4 +1,27 @@
 package com.example.umc10th.domain.review.controller;
 
+import com.example.umc10th.domain.member.dto.response.MemberResDTO;
+import com.example.umc10th.domain.review.dto.request.ReviewReqDTO;
+import com.example.umc10th.domain.review.dto.response.ReviewResDTO;
+import com.example.umc10th.domain.review.service.ReviewService;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import com.example.umc10th.global.apiPayload.code.GeneralSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping("/review")
+    public ApiResponse<ReviewResDTO.CreateReviewResDTO> getReview(
+            @RequestBody ReviewReqDTO dto
+    ){
+        BaseSuccessCode code = GeneralSuccessCode.OK;
+        return ApiResponse.onSuccess(code, reviewService.createReview(dto));
+    }
 }
--- src/main/java/com/example/umc10th/domain/review/dto/request/ReviewReqDTO.java
+++ src/main/java/com/example/umc10th/domain/review/dto/request/ReviewReqDTO.java
@@ -1,4 +1,21 @@
 package com.example.umc10th.domain.review.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewReqDTO {
+    @NotNull
+    private Double rating;
+
+    @NotBlank
+    private String content;
+
+    @NotBlank
+    private String storeName;
 }
