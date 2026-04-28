4ff060d8ccffecd41e717eff4ea119f065b92402
yunnij <blrdyunnij@gmail.com>
2026-04-28 10:59:39 +0900

    [Feat] Home 조회 api

--- src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
+++ src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
@@ -1,4 +1,30 @@
 package com.example.umc10th.domain.member.controller;
 
+import com.example.umc10th.domain.member.dto.response.MemberResDTO;
+import com.example.umc10th.domain.member.service.MemberService;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import com.example.umc10th.global.apiPayload.code.GeneralErrorCode;
+import com.example.umc10th.global.apiPayload.code.GeneralSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/home")
+    public ApiResponse<MemberResDTO.HomeResDTO> getHome(
+            @RequestParam String region,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ){
+        BaseSuccessCode code = GeneralSuccessCode.OK;
+        return ApiResponse.onSuccess(code, memberService.getHome(region, page, size));
+    }
 }
--- src/main/java/com/example/umc10th/domain/member/dto/response/MemberResDTO.java
+++ src/main/java/com/example/umc10th/domain/member/dto/response/MemberResDTO.java
@@ -1,4 +1,43 @@
 package com.example.umc10th.domain.member.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+
 public class MemberResDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class HomeResDTO {
+        private String region;
+        private int totalPoint;
+
+        private int completedMission;
+        private int goalMission;
+
+        private int regionTotalMission;
+        private int regionCompletedMission;
+
+        private Page<MissionDTO> missions;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MissionDTO {
+        private Long missionId;
+        private String missionName;
+        private String storeName;
+        private String storeType;
+        private int point;
+        private int daysLeft;
+        private LocalDateTime endAt;
+    }
 }
