3f0a5e92ed2db72264c5dc2dbbe5f28b278d0111
yunnij <blrdyunnij@gmail.com>
2026-04-28 13:55:52 +0900

    [Feat] excetion 처리

--- src/main/java/com/example/umc10th/domain/food/exception/FoodException.java
+++ src/main/java/com/example/umc10th/domain/food/exception/FoodException.java
@@ -1,7 +1,10 @@
 package com.example.umc10th.domain.food.exception;
 
-public class FoodException extends RuntimeException {
-    public FoodException(String message) {
-        super(message);
+import com.example.umc10th.global.apiPayload.code.BaseErrorCode;
+import com.example.umc10th.global.apiPayload.exception.ProjectException;
+
+public class FoodException extends ProjectException {
+    public FoodException(BaseErrorCode errorCode) {
+        super(errorCode);
     }
 }
--- src/main/java/com/example/umc10th/global/apiPayload/code/GeneralErrorCode.java
+++ src/main/java/com/example/umc10th/global/apiPayload/code/GeneralErrorCode.java
@@ -20,7 +20,9 @@ public enum GeneralErrorCode implements BaseErrorCode{
             "COMMON404_1",
             "해당 리소스를 찾을 수 없습니다."),
 
-    ;
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,
+            "COMMON500_1",
+            "서버에 에러가 발생하였습니다.");
 
     private final HttpStatus status;
     private final String code;
