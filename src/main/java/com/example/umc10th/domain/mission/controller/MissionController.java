06d823bf196909adb5472f075687d774c46ecf0d
yunnij <blrdyunnij@gmail.com>
2026-04-28 11:14:01 +0900

    [Feat] mission 조회 api

--- src/main/java/com/example/umc10th/domain/mission/controller/MissionController.java
+++ src/main/java/com/example/umc10th/domain/mission/controller/MissionController.java
@@ -1,4 +1,33 @@
 package com.example.umc10th.domain.mission.controller;
 
+import com.example.umc10th.domain.member.dto.response.MemberResDTO;
+import com.example.umc10th.domain.mission.dto.response.MissionResDTO;
+import com.example.umc10th.domain.mission.enums.MissionStatus;
+import com.example.umc10th.domain.mission.service.MissionService;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import com.example.umc10th.global.apiPayload.code.GeneralSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class MissionController {
+
+    private final MissionService missionService;
+
+    @GetMapping("/mission")
+    public ApiResponse<MissionResDTO> getMission(
+            @RequestParam String region,
+            @RequestParam MissionStatus status,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ){
+        BaseSuccessCode code = GeneralSuccessCode.OK;
+        return ApiResponse.onSuccess(code, missionService.getMission(region, status, page, size));
+    }
 }
