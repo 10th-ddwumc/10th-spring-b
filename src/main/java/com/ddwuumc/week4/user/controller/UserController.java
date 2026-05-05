59facaef4034af0c82e599ebd42adad202be83c4
mjttong <145860909+mjttong@users.noreply.github.com>
2026-05-05 22:47:15 +0900

    ✨ feat: 홈화면 및 마이페이지 조회 기능 구현

--- src/main/java/com/ddwuumc/week4/user/controller/UserController.java
+++ src/main/java/com/ddwuumc/week4/user/controller/UserController.java
@@ -2,16 +2,14 @@ package com.ddwuumc.week4.user.controller;
 
 import com.ddwuumc.week4.global.code.GeneralCode;
 import com.ddwuumc.week4.global.common.ApiResponse;
+import com.ddwuumc.week4.mission.dto.MissionResponseDto.MissionList;
 import com.ddwuumc.week4.user.dto.UserRequestDto;
-import com.ddwuumc.week4.user.dto.UserResponseDto;
+import com.ddwuumc.week4.user.dto.UserResponseDto.MyPage;
 import com.ddwuumc.week4.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,9 +17,15 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/home")
-    public ResponseEntity<ApiResponse<UserResponseDto.Home>> getUserHome() {
+    public ResponseEntity<ApiResponse<MissionList>> getUserHome(@RequestParam(value = "page") Long page) {
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.onSuccess(GeneralCode.OK, userService.getUserHome()));
+                .body(ApiResponse.onSuccess(GeneralCode.OK, userService.getUserHome(page)));
+    }
+
+    @GetMapping("/api/mypage")
+    public ResponseEntity<ApiResponse<MyPage>> getUserMyPage() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.onSuccess(GeneralCode.OK, userService.getUserMyPage()));
     }
 
     @PostMapping("/api/auth/signup")
--- src/main/java/com/ddwuumc/week4/user/dto/UserResponseDto.java
+++ src/main/java/com/ddwuumc/week4/user/dto/UserResponseDto.java
@@ -1,31 +1,10 @@
 package com.ddwuumc.week4.user.dto;
 
-import com.ddwuumc.week4.mission.entity.MissionStatus;
-
-import java.util.List;
-
 public class UserResponseDto {
-    public record Home(
-            List<MissionItem> missions,
-            long successCount
-    ) {}
-
-    public record MissionItem(
-            MissionStatus status,
-            MissionInfo mission,
-            StoreInfo store
-    ) {}
-
-    public record MissionInfo(
-            Long id,
-            String period,
-            Integer price,
-            Integer point
-    ) {}
-
-    public record StoreInfo(
+    public record MyPage(
             String name,
-            String address,
-            String type
+            String email,
+            String phone,
+            Integer point
     ) {}
 }
