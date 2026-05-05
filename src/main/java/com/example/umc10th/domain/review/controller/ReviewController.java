f616b974b9c69193226f05b06bfc901b79584382
minetoiles <byeol9253@gmail.com>
2026-05-05 23:34:27 +0900

    feat: 리뷰 응답/에러 코드 생성

--- src/main/java/com/example/umc10th/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/umc10th/domain/review/controller/ReviewController.java
@@ -2,6 +2,7 @@ package com.example.umc10th.domain.review.controller;
 
 import com.example.umc10th.domain.review.dto.ReviewReqDTO;
 import com.example.umc10th.domain.review.dto.ReviewResDTO;
+import com.example.umc10th.domain.review.exception.code.ReviewSuccessCode;
 import com.example.umc10th.domain.review.service.ReviewService;
 import com.example.umc10th.global.apiPayload.ApiResponse;
 import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
@@ -20,7 +21,7 @@ public class ReviewController {
             ReviewReqDTO.reviewReq request
 
     ){
-        BaseSuccessCode code = GeneralSuccessCode.OK;
+        BaseSuccessCode code = ReviewSuccessCode.REVIEW_CREATED;
         return ApiResponse.onSuccess(code, reviewService.review(request));
     }
 }
