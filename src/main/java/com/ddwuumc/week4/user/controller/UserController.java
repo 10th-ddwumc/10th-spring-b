f5fdd46f590b079958cb4547612d1c0cb30eec3d
mjttong <145860909+mjttong@users.noreply.github.com>
2026-05-12 16:06:13 +0900

    🐛 fix: 오프셋 페이징 기반 홈화면 조회 기능 수정

--- src/main/java/com/ddwuumc/week4/user/controller/UserController.java
+++ src/main/java/com/ddwuumc/week4/user/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.ddwuumc.week4.user.controller;
 
-import com.ddwuumc.week4.global.code.GeneralCode;
+import com.ddwuumc.week4.global.code.error.GeneralSuccessCode;
 import com.ddwuumc.week4.global.common.ApiResponse;
-import com.ddwuumc.week4.mission.dto.MissionResponseDto.MissionList;
+import com.ddwuumc.week4.global.common.PageDto.Offset;
+import com.ddwuumc.week4.mission.dto.MissionResponseDto.MissionItem;
 import com.ddwuumc.week4.user.dto.UserRequestDto;
 import com.ddwuumc.week4.user.dto.UserResponseDto.MyPage;
 import com.ddwuumc.week4.user.service.UserService;
@@ -17,20 +18,21 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/home")
-    public ResponseEntity<ApiResponse<MissionList>> getUserHome(@RequestParam(value = "page") Long page) {
+    public ResponseEntity<ApiResponse<Offset<MissionItem>>> getUserHome(@RequestParam(defaultValue = "0") Integer pageNumber,
+                                                                        @RequestParam(defaultValue = "0") Integer pageSize) {
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.onSuccess(GeneralCode.OK, userService.getUserHome(page)));
+                .body(ApiResponse.onSuccess(GeneralSuccessCode.OK, userService.getUserHome(pageNumber, pageSize)));
     }
 
     @GetMapping("/api/mypage")
     public ResponseEntity<ApiResponse<MyPage>> getUserMyPage() {
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.onSuccess(GeneralCode.OK, userService.getUserMyPage()));
+                .body(ApiResponse.onSuccess(GeneralSuccessCode.OK, userService.getUserMyPage()));
     }
 
     @PostMapping("/api/auth/signup")
     public ResponseEntity<ApiResponse<Long>> signup(@RequestBody UserRequestDto.SignupUser user) {
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ApiResponse.onSuccess(GeneralCode.CREATED, userService.signup(user)));
+                .body(ApiResponse.onSuccess(GeneralSuccessCode.CREATED, userService.signup(user)));
     }
 }
--- src/main/java/com/ddwuumc/week4/user/service/UserService.java
+++ src/main/java/com/ddwuumc/week4/user/service/UserService.java
@@ -1,9 +1,9 @@
 package com.ddwuumc.week4.user.service;
 
-import com.ddwuumc.week4.global.code.GeneralCode;
+import com.ddwuumc.week4.global.code.success.GeneralErrorCode;
+import com.ddwuumc.week4.global.common.PageDto.Offset;
 import com.ddwuumc.week4.global.exception.ProjectException;
 import com.ddwuumc.week4.mission.dto.MissionResponseDto.MissionItem;
-import com.ddwuumc.week4.mission.dto.MissionResponseDto.MissionList;
 import com.ddwuumc.week4.mission.dto.UserMissionConverter;
 import com.ddwuumc.week4.mission.entity.UserMission;
 import com.ddwuumc.week4.mission.repository.UserMissionRepository;
@@ -12,6 +12,7 @@ import com.ddwuumc.week4.user.dto.UserResponseDto.MyPage;
 import com.ddwuumc.week4.user.entity.User;
 import com.ddwuumc.week4.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -29,21 +30,24 @@ public class UserService {
         return 100L; // 생성된 사용자 id
     }
 
-    public MissionList getUserHome(Long page) {
-        User user = userRepository.findById(0L).orElseThrow(() -> new ProjectException(GeneralCode.NOT_FOUND));
-        Pageable pageable = PageRequest.of(0, 10);
-        List<UserMission> userMissionList = userMissionRepository.findHomePage(0L, user.getDetailAddress(), page, pageable);
+    public Offset<MissionItem> getUserHome(Integer pageNumber, Integer pageSize) {
+        User user = userRepository.findById(0L).orElseThrow(() -> new ProjectException(GeneralErrorCode.NOT_FOUND));
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        Page<UserMission> userMissionPage = userMissionRepository.findHomePage(0L, user.getDetailAddress(), pageable);
 
-        List<MissionItem> missions = userMissionList.stream()
-                .map(UserMissionConverter::convert)
-                .toList();
+        List<MissionItem> missions = UserMissionConverter.convert(userMissionPage);
 
-        return new MissionList(missions, missions.size());
+        return new Offset<>(
+                missions,
+                pageNumber,
+                pageSize,
+                userMissionPage.getTotalElements(),
+                userMissionPage.getTotalPages());
     }
 
     public MyPage getUserMyPage() {
         // TODO: user id는 하드코딩함
-        User user = userRepository.findById(0L).orElseThrow(() -> new ProjectException(GeneralCode.NOT_FOUND));
+        User user = userRepository.findById(0L).orElseThrow(() -> new ProjectException(GeneralErrorCode.NOT_FOUND));
         return new MyPage(user.getName(), user.getEmail(), user.getPhone(), user.getPoint());
     }
 }
